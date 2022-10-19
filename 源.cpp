8f4ed014c7dd56da55d603fe70b5695a011eae00
zhumj <z421789838@gmail.com>
2022-10-19 15:05:25 +0800

    添加背景图片

--- 源.cpp
+++ 源.cpp
@@ -1,4 +1,5 @@
 #include <graphics.h>
+#include <easyx.h>
 #include <conio.h>//_getch();
 #include "rose.h"
 #include <mmsystem.h>//mci库头文件
@@ -17,6 +18,14 @@ int main()
 	mciSendString(_T("open 往后余生-马良.mp3 alias love"), 0, 0, 0);
 	mciSendString(_T("play love repeat"), 0, 0, 0);//重复播放
 
+	//背景图片
+	IMAGE background;//定义一个图片名.
+	TCHAR image[1][22] = {
+		_T("background_image.jpg")
+	};
+	loadimage(&background, image[0], 930, 530, true);//从图片文件获取图像
+	putimage(0, 0, &background);//绘制图像到屏幕，图片左上角坐标为(0,0)
+
 	sayToYou();
 	drawRose();
 
@@ -29,7 +38,7 @@ int main()
 void sayToYou()
 {
 	int x = 550, y = 40;
-	TCHAR story[17][100] = {
+	TCHAR story[16][100] = {
 		_T("我一直认为山是水的故事"),
 		_T("云是风的故事"),
 		_T("你是我的故事"),
@@ -45,21 +54,22 @@ void sayToYou()
 		_T("心底温柔是你"),
 		_T("目光所致"),
 		_T("也是你"),
-		_T(""),
-		_T(" 亲爱的，生日快乐！")
+		_T("亲爱的，生日快乐！")
 	};
 
-	for (int i = 0; i < 17; i++)
+	setbkmode(TRANSPARENT);//文字背景透明
+
+	for (int i = 0; i < 16; i++)
 	{
 		int length = (lstrlen(story[i]) * sizeof(TCHAR)) / 2;
 		cout << length << endl;
 		for (int k = 0; k < length; k++)
 		{
-			if (i >= 16)
+			if (i >= 15)
 			{
 				settextstyle(36, 0, _T("Matura MT Script Capitals"));
 				settextcolor(RED);
-				outtextxy(x + k * 36, y + i * 25, story[i][k]);
+				outtextxy(x+k*36+36, y+i*25+25, story[i][k]);
 				Sleep(500);
 			}
 			else
@@ -67,7 +77,8 @@ void sayToYou()
 				//浪漫字体
 				settextstyle(23, 0, _T("Matura MT Script Capitals"));//字号，字宽，字体
 				//浪漫字体颜色（粉色）
-				settextcolor(RGB(0xdb, 0x70, 0x93));
+				//settextcolor(RGB(0xdb, 0x70, 0x93));
+				settextcolor(RGB(0x73, 0x33, 0x25));
 				outtextxy(x + k * 23, y + i * 25, story[i][k]);
 				Sleep(300);
 			}
